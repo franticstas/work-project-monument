dbb6aef6288f50680ce5ace2bbe2e695533c4b2b
Stas <franticstas@gmail.com>
2019-08-15 13:43:07 +0300

    add service page and service modal

--- src/js/script.js
+++ src/js/script.js
@@ -20,3 +20,19 @@ $('.mask').click(function() {
 });
 
 $('.form-phone').mask('+7 (999) 999-99-99');
+
+$('.products-block__item a').click(function(){
+  const imgLink = $(this).parent().find('img').attr('src');//Копируем адрес фото
+  const productName = $(this).parent().find('.products-block__name').text();//Копируем название
+  const productText = $(this).parent().find('.products-block__description').text();//Копируем описание
+
+  $('.modal__product-img img').attr('src', '');
+  $('.modal__product-img img').attr('src', imgLink);//Вставляем адрес фото в модалку
+  $('.modal__product .title-section').text(productName); //Вставляем название продукта в модалку
+  $('.modal__product .modal__product-text').text(productText); //Вставляем описание продукта в модалку
+  $('.modal__input-product-name').val(productName);
+
+  $(".modal__product .modal__product-table").empty(); //Очищаем блок с таблицей
+  $(this).parent().find('.products-block__specifications')
+    .clone().appendTo(".modal__product .modal__product-table"); //Компируем таблицу и продукта
+});
